9d38ba2dff022132aeebf0f497f48dba48813e19
jasonm10 <67562733+jasonm10@users.noreply.github.com>
2021-01-07 21:55:05 -0800

    1st commit

--- app/home/routes.py
+++ app/home/routes.py
@@ -5,39 +5,43 @@ Copyright (c) 2019 - present AppSeed.us
 
 from app.home import blueprint
 from flask import render_template, redirect, url_for, request
-from flask_login import login_required, current_user
+# from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
 
+
 @blueprint.route('/index')
-@login_required
+# @login_required
 def index():
 
     return render_template('index.html', segment='index')
 
+
 @blueprint.route('/<template>')
-@login_required
+# @login_required
 def route_template(template):
 
     try:
 
-        if not template.endswith( '.html' ):
+        if not template.endswith('.html'):
             template += '.html'
 
         # Detect the current page
-        segment = get_segment( request )
+        segment = get_segment(request)
 
         # Serve the file (if exists) from app/templates/FILE.html
-        return render_template( template, segment=segment )
+        return render_template(template, segment=segment)
 
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
+
     except:
         return render_template('page-500.html'), 500
 
-# Helper - Extract current page name from request 
-def get_segment( request ): 
+# Helper - Extract current page name from request
+
+
+def get_segment(request):
 
     try:
 
@@ -46,7 +50,7 @@ def get_segment( request ):
         if segment == '':
             segment = 'index'
 
-        return segment    
+        return segment
 
     except:
-        return None  
+        return None
